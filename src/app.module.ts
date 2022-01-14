4c1c72f7ea6069ca753dd54d9a0ab501e8bbcbf2
Dmitry Gongalo <dmitry.gongalo@student.lab.fi>
2022-01-14 22:40:54 +0200

    1

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TestModule } from './test/test.module';
+import { LogsModule } from './logs/logs.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [TestModule],
+  imports: [TestModule, LogsModule, TypeOrmModule.forRoot()],
   controllers: [AppController],
   providers: [AppService],
 })
